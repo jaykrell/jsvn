4e7e4c7577755171704b039029629af7b78b13af
jaykrell <jay.krell@cornell.edu>
2008-05-27 07:04:42 +0000

    use fewer environment variables

--- env/cm3/cm3.cygwin.bat
+++ env/cm3/cm3.cygwin.bat
@@ -3,10 +3,11 @@ call "%~dp0..\compose" "%~f0"
 set LIB=
 set INCLUDE=
 
-set CM3_GCC_BACKEND=yes
-set CM3_OSTYPE=POSIX
-set CM3_TARGET=NT386
-set OMIT_GCC=yes
-set CM3_ROOT=/dev2/cm3.2
+set CM3_GCC_BACKEND=
+set CM3_OSTYPE=
+set CM3_TARGET=
+set OMIT_GCC=
+set CM3_ROOT=/dev2/cm3
+if exist %SystemDrive%\dev2\cm3.2 set CM3_ROOT=/dev2/cm3.2
 set CM3_INSTALL=/cm3
-set M3CONFIG=/dev2/cm3.2/m3-sys/cminstall/src/config/NT386GNU
+set M3CONFIG=
--- env/cm3/cm3.mingwin.bat
+++ env/cm3/cm3.mingwin.bat
@@ -1,12 +1,14 @@
 call "%~dp0..\compose" "%~f0"
 call "%~dp0..\msys" "%~f0"
+@echo on
 set LIB=
 set INCLUDE=
 
-set CM3_GCC_BACKEND=yes
-set CM3_OSTYPE=WIN32
-set CM3_TARGET=NT386
-set OMIT_GCC=yes
-set CM3_ROOT=c:\dev2\cm3.2
-set CM3_INSTALL=c:\cm3
-set M3CONFIG=c:\dev2\cm3.2\m3-sys\cminstall\src\config\NT386
+set CM3_GCC_BACKEND=
+set CM3_OSTYPE=
+set CM3_TARGET=
+set OMIT_GCC=
+set CM3_ROOT=%SystemDrive%\dev2\cm3
+if exist %SystemDrive%\dev2\cm3.2 set CM3_ROOT=%SystemDrive%\dev2\cm3.2
+set CM3_INSTALL=%SystemDrive%\cm3
+set M3CONFIG=
