b010ffc9e0a521765a7472c6301fa3ad37070845
jaykrell <jay.krell@cornell.edu>
2008-07-23 10:34:50 +0000

    new files

--- env/djgpp203.bat
+++ env/djgpp203.bat
@@ -5,19 +5,19 @@ call \dev2\j\env\clearenv
 @rem if "%COMSPEC%" == "Z:\COMMAND.COM" set PATH=z:\
 @rem if not "%COMSPEC%" == "Z:\COMMAND.COM" set PATH=%WINDIR%\system32;%WINDIR%;%WINDIR%\System32\Wbem
 
-@if exist c:\djgpp\203\nul goto :c
-@if exist c:\djgpp\203     goto :c
-@if exist d:\djgpp\203\nul goto :d
-@if exist d:\djgpp\203     goto :d
+@if exist c:\djgpp.203\nul goto :c
+@if exist c:\djgpp.203     goto :c
+@if exist d:\djgpp.203\nul goto :d
+@if exist d:\djgpp.203     goto :d
 @echo \djgpp\203 not found
 @goto :end
 
 :c
-@set DJGPP=c:\djgpp\203
+@set DJGPP=c:\djgpp.203
 @goto :common
 
 :d
-@set DJGPP=d:\djgpp\203
+@set DJGPP=d:\djgpp.203
 @goto :common
 
 :common
