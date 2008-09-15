a3a108a09f7088c80fbf29e873d099cb16aa303b
jaykrell <jay.krell@cornell.edu>
2008-09-15 14:11:19 +0000

    update

--- env/Cygwin.bat
+++ env/Cygwin.bat
@@ -4,6 +4,7 @@ call %SystemDrive%\dev2\j\env\clearenv
 
 set PATH=%SystemDrive%\cygwin\usr\X11R6\bin;%PATH%
 set PATH=%SystemDrive%\cygwin\bin;%PATH%
+set PATH=%SystemDrive%\cygwin\usr\local\bin;%PATH%
 set CYGWIN=server
 
 @rem reset errorlevel
