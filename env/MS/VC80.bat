27ace2ff7334ace194a140f6e418342a697f233d
jaykrell <jaykrell@d79e31fd-f33a-0410-88ef-ed378667c97f>
2008-09-15 14:10:48 +0000

    update

--- env/MS/VC80.bat
+++ env/MS/VC80.bat
@@ -2,8 +2,8 @@
 
 @echo on
 
-set INCLUDE=%SystemDrive%\msdev\80\VC\include;%SystemDrive%\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Include
-set LIB=%SystemDrive%\msdev\80\VC\lib;%SystemDrive%\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib
+set INCLUDE=%SystemDrive%\msdev\80\VC\include;%SystemDrive%\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Include;%SystemDrive%\Program Files\Microsoft SDKs\Windows\v6.0A\Include
+set LIB=%SystemDrive%\msdev\80\VC\lib;%SystemDrive%\Program Files\Microsoft Platform SDK for Windows Server 2003 R2\Lib;%SystemDrive%\Program Files\Microsoft SDKs\Windows\v6.0A\lib
 set PATH=%SystemDrive%\msdev\80\VC\BIN;%SystemDrive%\msdev\80\Common7\IDE;%PATH%
 
 @rem reset errorlevel
