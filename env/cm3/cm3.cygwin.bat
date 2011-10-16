f0db6285e940770ed3ef7323134bdd3298b7dea6
jaykrell <jaykrell@d79e31fd-f33a-0410-88ef-ed378667c97f>
2011-10-16 22:15:39 +0000

    update

--- env/cm3/cm3.cygwin.bat
+++ env/cm3/cm3.cygwin.bat
@@ -11,3 +11,5 @@ set CM3_ROOT=/dev2/cm3
 if exist %SystemDrive%\dev2\cm3.2 set CM3_ROOT=/dev2/cm3.2
 set CM3_INSTALL=/cm3
 set M3CONFIG=
+rem set M3CONFIG=/dev2/cm3/m3-sys/cminstall/src/config-no-install/NT386GNU
+set CM3_TARGET=NT386GNU
